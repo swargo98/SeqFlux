0f60627fe271ca1f9ace0b09bf90355ddc7dbbec
Rasman Mubtasim Swargo <47280104+swargo98@users.noreply.github.com>
2026-05-22 00:16:27 -0500

    Add shared utilities

---
+++ utils.py
@@ -0,0 +1,78 @@
+import os
+import subprocess
+import time
+import numpy as np
+import shutil
+
+
+def tcp_stats(RCVR_ADDR, logger):
+    start = time.time()
+    sent, retm = 0, 0
+
+    try:
+        data = os.popen("ss -ti").read().split("\n")
+        for i in range(1,len(data)):
+            if RCVR_ADDR in data[i-1]:
+                parse_data = data[i].split(" ")
+                for entry in parse_data:
+                    if "data_segs_out" in entry:
+                        sent += int(entry.split(":")[-1])
+
+                    if "bytes_retrans" in entry:
+                        pass
+
+                    elif "retrans" in entry:
+                        retm += int(entry.split("/")[-1])
+
+    except Exception as e:
+        print(e)
+
+    end = time.time()
+    logger.debug("Time taken to collect tcp stats: {0}ms".format(np.round((end-start)*1000)))
+    return sent, retm
+
+
+def available_space(path="/dev/shm/data/"):
+    try:
+        space = shutil.disk_usage(path)
+        return (np.round(space.used/10**9,3), np.round(space.free/10**9,3))
+    except:
+        return (0,0)
+
+
+def available_space_bytes(path="/dev/shm/data/"):
+    try:
+        return shutil.disk_usage(path).free
+    except Exception:
+        return 0
+
+
+def get_dir_size(logger, path="/dev/shm/"):
+    total = 0
+    try:
+        with os.scandir(path) as iter:
+            for entry in iter:
+                if entry.is_file():
+                    total += entry.stat().st_size
+                elif entry.is_dir():
+                    total += get_dir_size(entry.path)
+
+        total = np.round(total/10**9, 3)
+        logger.debug(f"Path={path} -- Size={total} GB")
+    except Exception as e:
+        logger.debug(e)
+
+    return total
+
+
+def run(cmd, logger):
+    try:
+        cmd_output = subprocess.run(cmd.split())
+
+        logger.debug(f'[{cmd!r} exited with {cmd_output.returncode}]')
+        if cmd_output.stdout:
+            logger.debug(f'[stdout]\fcount{cmd_output.decode()}')
+        if cmd_output.stderr:
+            logger.debug(f'[stderr]\fcount{cmd_output.decode()}')
+    except Exception as e:
+        logger.debug(e)
